a737ba6cce8cf7218646d8bd77b8f8a7a6c5ab82
melkev <kevin.melliet.3@gmail.com>
2020-11-05 16:52:29 +0100

    update navbar with tailwindcss

--- components/shared/Navbar.vue
+++ components/shared/Navbar.vue
@@ -1,85 +1,126 @@
 <template>
-  <nav class="navbar is-active" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <nuxt-link class="navbar-item" to="/">
-        <h1 class="brand-title">Pearl-hub</h1>
+  <header
+    class="bg-gray-100 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 sm:h-16 s"
+  >
+    <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+      <nuxt-link class="text-gray-800 font-semibold rounded " to="/">
+        <h1
+          class="mt-1 block px-2 text-xl sm:text-2xl  sm:mt-0 sm:ml-2 md:text-3xl"
+        >
+          Pearl-hub
+        </h1>
       </nuxt-link>
-      <!-- Adds click to open -->
-      <!-- Adds active class -->
-      <a
-        @click="() => {}"
-        role="button"
-        class="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
+      <div class="sm:hidden">
+        <button
+          @click="isOpen = !isOpen"
+          type="button"
+          class="block text-gray-800 hover:text-gray-800 focus:text-gray-800 focus:outline-none"
+        >
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              v-if="isOpen"
+              fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+            />
+            <path
+              v-if="!isOpen"
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav
+      :class="isOpen ? 'block' : 'hidden'"
+      class="px-2 pt-2 pb-4 sm:flex sm:p-0"
+    >
+      <nuxt-link
+        to="/"
+        class="block px-2 py-1 text-gray-800 font-semibold rounded hover:bg-gray-500"
       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+        Home
+      </nuxt-link>
+      <nuxt-link
+        to="#"
+        class="block px-2 py-1 text-gray-800 font-semibold rounded hover:bg-gray-500"
+      >
+        Courses
+      </nuxt-link>
+      <nuxt-link
+        to="#"
+        class="block px-2 py-1 text-gray-800 font-semibold rounded hover:bg-gray-500"
+      >
+        Blogs
+      </nuxt-link>
+      <nuxt-link
+        to="#"
+        class="block px-2 py-1 text-gray-800 font-semibold rounded hover:bg-gray-500"
+      >
+        About
+      </nuxt-link>
+      <nuxt-link
+        to="#"
+        class="block px-2 py-1 text-gray-800 font-semibold rounded hover:bg-gray-500"
+      >
+        Cv
+      </nuxt-link>
 
-    <!-- Adds active class -->
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <nuxt-link to="/" class="navbar-item">
-          Home
-        </nuxt-link>
-        <nuxt-link to="#" class="navbar-item">
-          Courses
-        </nuxt-link>
-        <nuxt-link to="#" class="navbar-item">
-          Blogs
-        </nuxt-link>
-        <nuxt-link to="#" class="navbar-item">
-          About
-        </nuxt-link>
-        <nuxt-link to="#" class="navbar-item">
-          Cv
-        </nuxt-link>
-        
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <!-- If Authenticated -->
-            <template v-if="isAuth">
-              <figure class="image avatar is-32x32 m-r-sm">
-                <img class="is-rounded" :src="user.avatar" />
-              </figure>
-              <div class="m-r-sm m-b-sm">Welcome {{ user.username }}</div>
-              <!-- If Admin -->
-              <button
-                v-if="isAdmin"
-                class="button is-link is-outlined"
-                @click="() => $router.push('/instructor')"
-              >
-                Instructor
-              </button>
-              <a class="button is-primary" @click="logout">
-                Logout
-              </a>
-            </template>
-            <template v-else>
-              <nuxt-link to="/register" class="button is-primary">
-                Sign up
-              </nuxt-link>
-              <nuxt-link to="/login" class="button is-light">
-                Log in
-              </nuxt-link>
-            </template>
+      <!--  -->
+      <div class="flex justify-around items-center lg:ml-24 ">
+        <template v-if="isAuth">
+          <div class="hidden sm:flex items-center justify-center sm:mr-4">
+            <figure class="w-6">
+              <img class=" hidden lg:block rounded-2xl" :src="user.avatar" />
+            </figure>
+            <div class=" px-2 py-1 text-gray-800 font-semibold rounded ">
+              Welcome {{ user.username }}
+            </div>
           </div>
-        </div>
+          <!-- If Admin -->
+          <button
+            v-if="isAdmin"
+            class="w-1/3 text-center text-sm px-2 py-1 rounded-xl font-semibold text-gray-800 hover:bg-gray-500  mt-4 sm:w-20 sm:ml-3 sm:mt-0"
+            @click="() => $router.push('/instructor')"
+          >
+            Instructor
+          </button>
+          <a
+            class="w-1/3 text-center text-sm px-2 py-1 rounded-xl font-semibold text-gray-800 hover:bg-gray-500  mt-4 sm:w-20 sm:ml-3 sm:mt-0"
+            @click="logout"
+          >
+            Logout
+          </a>
+        </template>
+
+        <!--  -->
+        <template v-else>
+          <nuxt-link
+            to="/register"
+            class="w-1/3 text-center text-sm px-2 py-1 rounded-xl font-semibold text-gray-800 hover:bg-gray-500 mt-4 sm:w-20 sm:ml-3 sm:mt-0 "
+          >
+            Sign up
+          </nuxt-link>
+          <nuxt-link
+            to="/login"
+            class="w-1/3 text-center text-sm px-2 py-1 rounded-xl font-semibold text-gray-800 hover:bg-gray-500  mt-4 sm:w-20 sm:ml-3 sm:mt-0 "
+          >
+            Log in
+          </nuxt-link>
+        </template>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
   computed: {
     ...mapGetters({
       user: "auth/authUser",
@@ -88,9 +129,10 @@ export default {
     })
   },
   methods: {
-    logout(){
-      this.$store.dispatch('auth/logout')
-        .then(()=> this.$router.push('/login'))
+    logout() {
+      this.$store
+        .dispatch("auth/logout")
+        .then(() => this.$router.push("/login"));
     }
   }
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,13 +30,13 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build"
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    "@nuxtjs/tailwindcss",
+    
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios"
   ],
