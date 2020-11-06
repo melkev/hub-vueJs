664e0aa917edf2b1aa72c38dd388e52e6890a679
melkev <kevin.melliet.3@gmail.com>
2020-11-06 19:43:53 +0100

    update register page with tailwind

--- pages/README.md
+++ pages/README.md
@@ -4,3 +4,163 @@ This directory contains your Application Views and Routes.
 The framework reads all the `*.vue` files inside this directory and creates the router of your application.
 
 More information about the usage of this directory in [the documentation](https://nuxtjs.org/guide/routing).
+<section class="container ">
+    <div class="flex items-center justify-center mt-4 ">
+      <div
+        class="w-10/12 sm:w-7/12 md:w-4/12 rounded overflow-hidden shadow-lg"
+      >
+        <div class=" px-4 py-2 ">
+          <h3 class="font-bold text-xl sm:text-2xl mb-1 text-center mr-40">Register</h3>
+          <p class=" text-lg sm:text-xl mb-2  text-center mr-8">Please register to proceed.</p>
+          <figure class="">
+              <img
+                class=" rounded-full h-32 mx-auto p-1 border-2 border-red-100"
+                src="https://via.placeholder.com/300"
+              />
+            </figure>
+            <div class="  -mt-16">
+          <div class=" sm:w-10/12 md:w-auto rounded  shadow-lg">
+            
+              <form class="flex flex-col items-center justify-center pt-16 ">
+                <div class="   ">
+                  <div class=" m-4  ">
+                    <input
+                      v-model="form.username"
+                      @blur="$v.form.username.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1"
+                      type="text"
+                      placeholder=" Username"
+                    />
+                    <div v-if="$v.form.username.$error" class=" m-2  font-normal">
+                      <span v-if="!$v.form.username.required" class="  "
+                        >Username is required</span
+                      >
+                      <span v-if="!$v.form.username.minLength" class=" "
+                        >Username minlenght is 6 caracters</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class=" ">
+                  <div class="m-4 ">
+                    <input
+                      v-model="form.name"
+                      @blur="$v.form.name.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1 "
+                      type="text"
+                      placeholder="Name"
+                    />
+                    <div v-if="$v.form.name.$error" class=" ">
+                      <span v-if="!$v.form.name.required" class=""
+                        >Name is required</span
+                      >
+                      <span v-if="!$v.form.name.minLength" class=""
+                        >name minimum length is 6 characters</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="">
+                  <div class="m-4">
+                    <input
+                      v-model="form.email"
+                      @blur="$v.form.email.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1"
+                      type="email"
+                      placeholder="Your Email"
+                    />
+                    <div v-if="$v.form.email.$error" class="">
+                      <span v-if="!$v.form.email.required" class=""
+                        >Email is required</span
+                      >
+                      <span v-if="!$v.form.email.emailValidator" class=""
+                        >Email address is not valid</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="">
+                  <div class="m-4">
+                    <input
+                      v-model="form.avatar"
+                      @blur="$v.form.avatar.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1"
+                      type="text"
+                      placeholder="Avatar"
+                      autocomplete=""
+                    />
+                    <div v-if="$v.form.avatar.$error" class="">
+                      <span v-if="!$v.form.avatar.url" class=""
+                        >Url format is not valid!</span
+                      >
+                      <span v-if="!$v.form.avatar.supportedFiledType" class=""
+                        >Selected file type is not valid!</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="">
+                  <div class="m-4">
+                    <input
+                      v-model="form.password"
+                      @blur="$v.form.password.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1"
+                      type="password"
+                      placeholder="Your Password"
+                      autocomplete="new-password"
+                    />
+                    <div v-if="$v.form.password.$error" class="">
+                      <span v-if="!$v.form.password.required" class=""
+                        >Password is required</span
+                      >
+                      <span v-if="!$v.form.password.minLength" class=""
+                        >Password minimum length is 6 letters</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="">
+                  <div class="m-4">
+                    <input
+                      v-model="form.passwordConfirmation"
+                      @blur="$v.form.passwordConfirmation.$touch()"
+                      class=" font-bold rounded-md shadow-lg p-1"
+                      type="password"
+                      placeholder="Password Confirmation"
+                      autocomplete="off"
+                    />
+                    <div v-if="$v.form.passwordConfirmation.$error" class="">
+                      <span
+                        v-if="!$v.form.passwordConfirmation.required"
+                        class=""
+                        >Password is required</span
+                      >
+                      <span
+                        v-if="!$v.form.passwordConfirmation.sameAsPassword"
+                        class=""
+                        >Password confirmation should be the same as
+                        password</span
+                      >
+                    </div>
+                  </div>
+                </div>
+                <button
+                  @click.prevent="register"
+                  :disabled="$v.form.$invalid"
+                  type="submit"
+                  class=""
+                >
+                  Register
+                </button>
+              </form>
+            </div>
+          </div>
+          <p class="">
+            <nuxt-link to="/login">Login</nuxt-link> &nbsp;·&nbsp;
+            <a>Sign Up With Google</a> &nbsp;·&nbsp;
+            <a href="../">Need Help?</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
--- pages/Register.vue
+++ pages/Register.vue
@@ -1,171 +1,185 @@
 <template>
-  <section class="hero is-success is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="column is-4 is-offset-4">
-          <h3 class="title has-text-grey">Register</h3>
-          <p class="subtitle has-text-grey">Please register to proceed.</p>
-          <div class="box">
-            <figure class="avatar">
-              <img src="https://via.placeholder.com/300" />
-            </figure>
-            <form>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.username"
-                    @blur="$v.form.username.$touch()"
-                    class="input is-large"
-                    type="text"
-                    placeholder="Username"
-                  />
-                  <div v-if="$v.form.username.$error" class="form-error">
-                    <span
-                      v-if="!$v.form.username.required"
-                      class="help is-danger"
-                      >Username is required</span
-                    >
-                    <span
-                      v-if="!$v.form.username.minLength"
-                      class="help is-danger"
-                      >Username minlenght is 6 caracters</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.name"
-                    @blur="$v.form.name.$touch()"
-                    class="input is-large"
-                    type="text"
-                    placeholder="Name"
-                  />
-                  <div v-if="$v.form.name.$error" class="form-error">
-                    <span v-if="!$v.form.name.required" class="help is-danger"
-                      >Name is required</span
-                    >
-                    <span v-if="!$v.form.name.minLength" class="help is-danger"
-                      >name minimum length is 6 characters</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.email"
-                    @blur="$v.form.email.$touch()"
-                    class="input is-large"
-                    type="email"
-                    placeholder="Your Email"
-                  />
-                  <div v-if="$v.form.email.$error" class="form-error">
-                    <span v-if="!$v.form.email.required" class="help is-danger"
-                      >Email is required</span
-                    >
-                    <span
-                      v-if="!$v.form.email.emailValidator"
-                      class="help is-danger"
-                      >Email address is not valid</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.avatar"
-                    @blur="$v.form.avatar.$touch()"
-                    class="input is-large"
-                    type="text"
-                    placeholder="Avatar"
-                    autocomplete=""
-                  />
-                  <div v-if="$v.form.avatar.$error" class="form-error">
-                    <span v-if="!$v.form.avatar.url" class="help is-danger"
-                      >Url format is not valid!</span
-                    >
-                    <span
-                      v-if="!$v.form.avatar.supportedFiledType"
-                      class="help is-danger"
-                      >Selected file type is not valid!</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.password"
-                    @blur="$v.form.password.$touch()"
-                    class="input is-large"
-                    type="password"
-                    placeholder="Your Password"
-                    autocomplete="new-password"
-                  />
-                  <div v-if="$v.form.password.$error" class="form-error">
-                    <span
-                      v-if="!$v.form.password.required"
-                      class="help is-danger"
-                      >Password is required</span
-                    >
-                    <span
-                      v-if="!$v.form.password.minLength"
-                      class="help is-danger"
-                      >Password minimum length is 6 letters</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <input
-                    v-model="form.passwordConfirmation"
-                    @blur="$v.form.passwordConfirmation.$touch()"
-                    class="input is-large"
-                    type="password"
-                    placeholder="Password Confirmation"
-                    autocomplete="off"
-                  />
-                  <div
-                    v-if="$v.form.passwordConfirmation.$error"
-                    class="form-error"
-                  >
-                    <span
-                      v-if="!$v.form.passwordConfirmation.required"
-                      class="help is-danger"
-                      >Password is required</span
-                    >
-                    <span
-                      v-if="!$v.form.passwordConfirmation.sameAsPassword"
-                      class="help is-danger"
-                      >Password confirmation should be the same as
-                      password</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <button
-                @click.prevent="register"
-                :disabled="$v.form.$invalid"
-                type="submit"
-                class="button is-block is-info is-large is-fullwidth"
-              >
-                Register
-              </button>
-            </form>
+  <div class="w-11/12 sm:w-full max-w-2xl mx-auto mt-5">
+    <form class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            username
+          </label>
+          <input
+            v-model="form.username"
+            @blur="$v.form.username.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-800 border border-gray-500 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+          />
+          <div
+            v-if="$v.form.username.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.username.required" class="  "
+              >Username is required</span
+            >
+            <span v-if="!$v.form.username.minLength" class=" "
+              >Username minlenght is 6 caracters</span
+            >
           </div>
-          <p class="has-text-grey">
-            <nuxt-link to="/login">Login</nuxt-link> &nbsp;·&nbsp;
-            <a>Sign Up With Google</a> &nbsp;·&nbsp;
-            <a href="../">Need Help?</a>
-          </p>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            Name
+          </label>
+          <input
+            v-model="form.name"
+            @blur="$v.form.name.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-800 border mb-2 border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+          />
+          <div
+            v-if="$v.form.name.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.name.required">Name is required</span>
+            <span v-if="!$v.form.name.minLength" class=""
+              >name minimum length is 6 characters</span
+            >
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="email"
+          >
+            email
+          </label>
+          <input
+            v-model="form.email"
+            @blur="$v.form.email.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="email"
+            type="email"
+            placeholder="example@gmail.com"
+          />
+          <div
+            v-if="$v.form.email.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.email.required">Email is required</span>
+            <span v-if="!$v.form.email.emailValidator"
+              >Email address is not valid</span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="avatar"
+          >
+            avatar
+          </label>
+          <input
+            v-model="form.avatar"
+            @blur="$v.form.avatar.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="avatar"
+            type="text"
+            placeholder="Avatar"
+            autocomplete=""
+          />
+          <div
+            v-if="$v.form.avatar.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.avatar.url" class=""
+              >Url format is not valid!</span
+            >
+            <span v-if="!$v.form.avatar.supportedFiledType" class=""
+              >Selected file type is not valid!</span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="password"
+          >
+            password
+          </label>
+          <input
+            v-model="form.password"
+            @blur="$v.form.password.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white"
+            id="password"
+            type="password"
+            autocomplete="new-password"
+            placeholder="*********"
+          />
+          <div
+            v-if="$v.form.password.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.password.required" class=""
+              >Password is required</span
+            >
+            <span v-if="!$v.form.password.minLength" class=""
+              >Password minimum length is 6 letters</span
+            >
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="confirmpasword"
+          >
+            confirm Password
+          </label>
+          <input
+            v-model="form.passwordConfirmation"
+            @blur="$v.form.passwordConfirmation.$touch()"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="confirmpasword"
+            type="password"
+            placeholder="********"
+            autocomplete="off"
+          />
+          <div
+            v-if="$v.form.passwordConfirmation.$error"
+            class="ml-2 text-red-500 text-xs italic"
+          >
+            <span v-if="!$v.form.passwordConfirmation.required" class=""
+              >Password is required</span
+            >
+            <span v-if="!$v.form.passwordConfirmation.sameAsPassword" class=""
+              >Password confirmation should be the same as password</span
+            >
+          </div>
+        </div>
+      </div>
+      <button
+        @click.prevent="register"
+        :disabled="$v.form.$invalid"
+        type="submit"
+        class=""
+      >
+        Register
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -174,7 +188,7 @@ import {
   email,
   minLength,
   url,
-  sameAs,
+  sameAs
 } from "vuelidate/lib/validators";
 import { supportedFiledType } from "@/helpers/validators";
 export default {
@@ -186,42 +200,42 @@ export default {
         name: null,
         avatar: null,
         password: null,
-        passwordConfirmation: null,
-      },
+        passwordConfirmation: null
+      }
     };
   },
   validations: {
     form: {
       username: {
         required,
-        minLength: minLength(6),
+        minLength: minLength(6)
       },
       name: {
         required,
-        minLength: minLength(6),
+        minLength: minLength(6)
       },
       email: {
         required,
-        emailValidator: email,
+        emailValidator: email
       },
       avatar: {
         url,
-        supportedFiledType,
+        supportedFiledType
       },
       password: {
         required,
-        minLength: minLength(6),
+        minLength: minLength(6)
       },
       passwordConfirmation: {
         required,
-        sameAsPassword: sameAs("password"),
-      },
-    },
+        sameAsPassword: sameAs("password")
+      }
+    }
   },
   computed: {
     isFormValid() {
       return !this.$v.form.$invalid;
-    },
+    }
   },
   methods: {
     /**
@@ -233,45 +247,21 @@ export default {
         this.$store
           .dispatch("auth/register", this.form)
           .then(() => this.$router.push("/login"))
-          .catch((err) =>
+          .catch(err =>
             this.$toasted.error(
               " Uuuups , unexpected issue please try to regiter again",
               { duration: 3000 }
             )
           );
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-.hero.is-success {
-  background: #f2f6fa;
-}
-.hero .nav,
-.hero.is-success .nav {
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
-.box {
-  margin-top: 5rem;
-}
 .avatar {
   margin-top: -70px;
   padding-bottom: 20px;
-}
-.avatar img {
-  height: 128px;
-  width: 128px;
-  padding: 5px;
-  background: #fff;
-  border-radius: 50%;
-  -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
-    0 0 0 1px rgba(10, 10, 10, 0.1);
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-}
-p.subtitle {
-  padding-top: 1rem;
 }
 </style>
